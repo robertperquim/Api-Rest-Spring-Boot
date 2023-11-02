4f7870e6d102d15cb891a75d50e286d54b07da92
robertperquim <perquimrobert@gmail.com>
2023-11-02 06:09:50 -0300

    Fix: Corrigindo erro de camelcase

--- src/main/java/med/voll/api/address/AddressDate.java
+++ src/main/java/med/voll/api/address/AddressDate.java
@@ -1,4 +1,4 @@
 package med.voll.api.address;
 
-public record AddressDate(String street, String neighborhood, String ZipCode, String city, String uf, String number, String complement) {
+public record AddressDate(String street, String neighborhood, String zipCode, String city, String uf, String number, String complement) {
 }
