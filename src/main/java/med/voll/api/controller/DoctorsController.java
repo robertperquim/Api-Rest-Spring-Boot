8ab999196a946ba9cb4473436b516486f15d6ed1
robertperquim <perquimrobert@gmail.com>
2023-11-02 07:05:39 -0300

    Fix: Adicionando @Transational

--- src/main/java/med/voll/api/controller/DoctorsController.java
+++ src/main/java/med/voll/api/controller/DoctorsController.java
@@ -5,6 +5,7 @@ import med.voll.api.doctor.Doctor;
 import med.voll.api.doctor.DoctorRepository;
 import med.voll.api.doctor.MedicalRegisterData;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,6 +16,7 @@ public class DoctorsController {
     private DoctorRepository doctorRepository;
 
     @PostMapping
+    @Transactional
     public void register(@RequestBody MedicalRegisterData data){
 
        doctorRepository.save(new Doctor(data));
