90f23fcbca53af9290a8a41cbebf6c1349f62f01
robertperquim <perquimrobert@gmail.com>
2023-11-02 06:11:04 -0300

    Feat: Criando post para salvar no banco

--- src/main/java/med/voll/api/controller/DoctorsController.java
+++ src/main/java/med/voll/api/controller/DoctorsController.java
@@ -1,15 +1,22 @@
 package med.voll.api.controller;
 
 
+import med.voll.api.doctor.Doctor;
+import med.voll.api.doctor.DoctorRepository;
 import med.voll.api.doctor.MedicalRegisterData;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("medicos")
 public class DoctorsController {
 
+    @Autowired
+    private DoctorRepository doctorRepository;
+
     @PostMapping
     public void register(@RequestBody MedicalRegisterData data){
-        System.out.println(data);
+
+       doctorRepository.save(new Doctor(data));
     }
 }
