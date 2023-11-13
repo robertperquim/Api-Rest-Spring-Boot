981c089b72fc5091b5dc7d827668bc398107f0e9
robertperquim <perquimrobert@gmail.com>
2023-11-12 22:20:08 -0300

    Feat: Adicionando validacoes no DTO doctors com bean validation

--- src/main/java/med/voll/api/doctor/MedicalRegisterData.java
+++ src/main/java/med/voll/api/doctor/MedicalRegisterData.java
@@ -1,7 +1,31 @@
 package med.voll.api.doctor;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import med.voll.api.address.AddressDate;
 
 //RECORD -> PARA PADRAO DTO DATA TRANSFER OBJECT
-public record MedicalRegisterData(String name, String email, String crm, Speciality speciality, AddressDate addressDate ) {
+public record MedicalRegisterData(
+        //validacoes dos atributos usando o bean validation
+
+        @NotBlank
+        String name,
+        @NotBlank
+        @Email
+        String email,
+
+        @NotBlank
+        @Pattern(regexp = "\\d{4,6}") //significa que o crm sao digitos e sao de 4 a 6 digitos
+        String crm,
+
+        @NotNull // o NotBlank serve apenas para strings nesse caso speciality e um enum
+        Speciality speciality,
+
+        @NotNull
+        @Valid  // aqui AddresDate e outro DTO que precisa de validacao tbm
+        AddressDate addressDate
+) {
 }
