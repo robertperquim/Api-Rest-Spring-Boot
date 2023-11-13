6832f036e1ffcef8457a160a704006f3e95a560d
robertperquim <perquimrobert@gmail.com>
2023-11-13 04:33:30 -0300

    Feat: Criando metodo para retornar somente Active true

--- src/main/java/med/voll/api/doctor/DoctorRepository.java
+++ src/main/java/med/voll/api/doctor/DoctorRepository.java
@@ -1,8 +1,13 @@
 package med.voll.api.doctor;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
+    Page<Doctor> findAllByActiveTrue(Pageable pages);
 }
