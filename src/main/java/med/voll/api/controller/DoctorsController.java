5a8e04e1c93ffb2375669077ebb93907cd6e35e6
robertperquim <perquimrobert@gmail.com>
2023-11-13 01:20:02 -0300

    Feat: Adicionando metedo get com paginacao

--- src/main/java/med/voll/api/controller/DoctorsController.java
+++ src/main/java/med/voll/api/controller/DoctorsController.java
@@ -4,10 +4,15 @@ package med.voll.api.controller;
 import jakarta.validation.Valid;
 import med.voll.api.doctor.Doctor;
 import med.voll.api.doctor.DoctorRepository;
+import med.voll.api.doctor.MedicalListData;
 import med.voll.api.doctor.MedicalRegisterData;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping("doctors")
@@ -21,5 +26,10 @@ public class DoctorsController {
     public void register(@RequestBody @Valid MedicalRegisterData data){
 
        doctorRepository.save(new Doctor(data));
+    }
+
+    @GetMapping
+    public Page<MedicalListData> doctorList (@PageableDefault(size = 10, sort = {"name"}) Pageable pages){
+        return doctorRepository.findAll(pages).map(MedicalListData::new);
     }
 }
