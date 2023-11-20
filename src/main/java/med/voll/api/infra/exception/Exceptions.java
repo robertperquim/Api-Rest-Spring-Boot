ef29b02277c9624b1b7f4d0d6437b8db8c2eb40f
robertperquim <perquimrobert@gmail.com>
2023-11-20 01:35:39 -0300

    Feat: Tratando Principais Exceptions

--- src/main/java/med/voll/api/infra/exception/Exceptions.java
+++ src/main/java/med/voll/api/infra/exception/Exceptions.java
@@ -1,12 +1,17 @@
 package med.voll.api.infra.exception;
 
 import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.naming.AuthenticationException;
+import java.nio.file.AccessDeniedException;
 import java.util.List;
 
 @RestControllerAdvice
@@ -21,6 +26,31 @@ public class Exceptions {
     public ResponseEntity handle400Error(MethodArgumentNotValidException exception){
         List<FieldError> errors = exception.getFieldErrors();
         return ResponseEntity.badRequest().body(errors.stream().map(ErrorsValidationData::new).toList());
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity handle400Error(HttpMessageNotReadableException ex) {
+        return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity handleBadCredentialsError() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    public ResponseEntity handleAuthenticationError() {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Authentication failed");
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity handleAccessDeniedError() {
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body("Access denied");
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity handle500Error(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error: " +ex.getLocalizedMessage());
     }
 
     //dto para retornar o erro e a mensagem no erro 400
