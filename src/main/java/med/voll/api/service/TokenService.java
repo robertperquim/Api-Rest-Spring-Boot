cdbde2decfeb39402a68becadbcbc61e6dcf73a2
robertperquim <perquimrobert@gmail.com>
2023-11-19 23:51:54 -0300

    Feat: Adicionando service do token

---
+++ src/main/java/med/voll/api/service/TokenService.java
@@ -0,0 +1,30 @@
+package med.voll.api.service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import med.voll.api.models.user.User;
+import org.springframework.stereotype.Service;
+
+import java.time.*;
+
+@Service
+public class TokenService {
+
+    public String generateToken(User user){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256("123456");
+            return JWT.create()
+                    .withIssuer("API Voll.med")
+                    .withSubject(user.getLogin())
+                    .withExpiresAt(expirationDate())
+                    .sign(algorithm);
+        } catch (JWTCreationException exception){
+            throw new RuntimeException("error generating token ", exception);
+        }
+    }
+
+    private Instant expirationDate() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
