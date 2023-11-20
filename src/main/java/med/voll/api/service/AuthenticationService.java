1ad468af2a520e5478f3d8b47fb106e090f2bf33
robertperquim <perquimrobert@gmail.com>
2023-11-19 21:03:52 -0300

    Feat: Criando service autentication

---
+++ src/main/java/med/voll/api/service/AuthenticationService.java
@@ -0,0 +1,21 @@
+package med.voll.api.service;
+
+
+import med.voll.api.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByLogin(username);
+    }
+}
